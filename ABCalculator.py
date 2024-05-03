bb27882b38f6b4b01e6db6a1cca3e422288f8c9e
aelirennfin <solcyanide@gmail.com>
2024-05-03 09:58:48 +0500

    Создание файла программы калькулятора

---
+++ ABCalculator.py
@@ -0,0 +1,12 @@
+# примеры построения графиков
+
+import tkinter as tk
+
+# создание главного окна
+
+root = tk.Tk()
+root.geometry("280x300")
+root.title("A/B калькулятор")
+
+# запуск цикла mainloop
+root.mainloop()

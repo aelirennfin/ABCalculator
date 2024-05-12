ec499477dab6d746b6c83ab67b9abb04fea2d906
aelirennfin <solcyanide@gmail.com>
2024-05-12 15:35:21 +0500

    Добавление вывода возможных разбросов

--- ABCalculator.py
+++ ABCalculator.py
@@ -39,8 +39,8 @@ def popup_window(n1, c1, n2, c2):
     txtOutput.place(x=15, y=115, width=470, height=300)
     
     # Добавление заголовка
-    txtOutput.insert(tk.END, '                              Контрольная         Тестовая' + os.linesep)
-    txtOutput.insert(tk.END, '                              группа              группа' + os.linesep)
+    txtOutput.insert(tk.END, '                           Контрольная         Тестовая' + os.linesep)
+    txtOutput.insert(tk.END, '                           группа              группа' + os.linesep)
     txtOutput.insert(tk.END, ' _________________________________________________________' + os.linesep)
     
     # Добавление вывода конверсии и стандартного отклонения
@@ -53,6 +53,51 @@ def popup_window(n1, c1, n2, c2):
     sigma2 = math.sqrt(p2*(1-p2)/n2)
     txtOutput.insert(tk.END, 'Стандартное отклонение' + num_percent(sigma1)
         +'  ' + num_percent (sigma2) + os.linesep)
+    txtOutput.insert(tk.END, '_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _' + os.linesep)
+    
+    # Добавление вывода возможных разбросов
+    z1 = 1.96
+    lower1_95 = p1-z1*sigma1
+    if lower1_95 < 0:
+        lower1_95 = 0
+    upper1_95 = p1+z1*sigma1
+    if lower1_95 > 1:
+        lower1_95 = 1
+    
+    lower2_95 = p2-z1*sigma2
+    if lower2_95 < 0:
+        lower2_95 = 0
+    upper2_95 = p2+z1*sigma1
+    if lower2_95 > 1:
+        lower2_95 = 1
+        
+    txtOutput.insert(tk.END, '95% Возможный разброс  '+ os.linesep)
+    txtOutput.insert(tk.END, '                   От  ' + num_percent(lower1_95)
+        + '    ' + num_percent(lower2_95) + os.linesep)
+    txtOutput.insert(tk.END, '                   До  ' + num_percent(upper1_95)
+        + '    ' + num_percent(upper2_95) + os.linesep)
+    txtOutput.insert(tk.END, '_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _' + os.linesep)
+    
+    z2 = 2.575
+    lower1_99 = p1-z2*sigma1
+    if lower1_99 < 0:
+        lower1_99 = 0
+    upper1_99 = p1+z2*sigma1
+    if lower1_99 > 1:
+        lower1_99 = 1
+    
+    lower2_99 = p2-z2*sigma2
+    if lower2_99 < 0:
+        lower2_99 = 0
+    upper2_99 = p2+z1*sigma1
+    if lower2_99 > 1:
+        lower2_99 = 1
+        
+    txtOutput.insert(tk.END, '99% Возможный разброс  '+ os.linesep)
+    txtOutput.insert(tk.END, '                   От  ' + num_percent(lower1_99)
+        + '    ' + num_percent(lower2_99) + os.linesep)
+    txtOutput.insert(tk.END, '                   До  ' + num_percent(upper1_99)
+        + '    ' + num_percent(upper2_99) + os.linesep)
     txtOutput.insert(tk.END, '_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _' + os.linesep)
     
     # Добавление кнопки закрытия окна
